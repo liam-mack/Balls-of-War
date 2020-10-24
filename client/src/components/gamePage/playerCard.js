3b5f3601b8d56437b2ed811e1181278134f05d48
Mohammed <mohammedyasirsuara@cmail.carleton.ca>
2020-10-24 13:48:09 -0400

    console log player values

--- client/src/components/gamePage/playerCard.js
+++ client/src/components/gamePage/playerCard.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 
 class PlayerCard extends Component{
 
+    showStats=()=>{
+        console.log(this.props.player.value)
+    }
+
     render(){
         return (
           <div className={this.props.className} style={{width:'13rem', height:'350px'}}>
               <img className='card-img-top' src={this.props.player.image} alt='Card'/>
               <p>{this.props.player.name}</p>
-              <p>{this.props.player.value}</p>
+              <p onClick = {this.showStats}>{this.props.player.value}</p>
           </div>
         );
         

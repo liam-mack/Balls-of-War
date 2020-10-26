9aea59551425b6602d97ad688afb015f32ec3551
Mohammed <mohammedyasirsuara@cmail.carleton.ca>
2020-10-26 16:50:50 -0400

    Edited the front-end

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import Btn from "./components/lakers";
 import GamePage from "./components/gamePage/page";
+import Login from "./components/loginPage/login"
 function App() {
   return (
     <>

37c64bae77170a6fdc4b02c7091649fc81f638cd
shamik05 <64784024+shamik05@users.noreply.github.com>
2020-10-26 20:25:27 -0400

    Added seed files for all four teams

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({ force: true }).then(() => {
-  return SeedBomb();
-}).then(() => {
+db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT
@@ -33,4 +31,6 @@ db.sequelize.sync({ force: true }).then(() => {
 }
 );
 
-// 
+// { force: true }
+// return SeedBomb();
+// }).then(() => {
--- sql/seedBomb.js
+++ sql/seedBomb.js
@@ -1,9 +1,11 @@
 const Lakers = require("./seeds/seedLakers");
 const Rockets = require("./seeds/seedRockets");
+const Heat = require("./seeds/seedHeat");
+const Raptors = require("./seeds/seedRaptors");
 
 module.exports = function() {
   return Promise.all([
-    Lakers(), Rockets()
+    Lakers(), Rockets(), Heat(), Raptors()
   ]).then(() => {
     console.log("Seeded successfully")
   })

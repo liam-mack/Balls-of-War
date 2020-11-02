78c868589eeb49b4c1a1f86f732e03a43c4b2da9
shamik05 <64784024+shamik05@users.noreply.github.com>
2020-11-02 18:40:28 -0500

    Added basic passport

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,37 @@
 const db = require("../models");
+const { Op, Sequelize } = require("sequelize");
+const passport = require("../config/passport");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
+  // Using the passport.authenticate middleware with our local strategy.
+  // If the user has valid login credentials, send them to the members page.
+  // Otherwise the user will be sent an error
+  app.post("/api/login", passport.authenticate("local"), (req, res) => {
+    // Sending back a password, even a hashed password, isn't a good idea
+    res.json({
+      email: req.user.email,
+      id: req.user.id,
+    });
+  });
+
+  // Route for signing up a user. The user's password is automatically hashed and stored securely
+  // thanks to how we configured our Sequelize User Model.
+  // If the user is created successfully, proceed to log the user in otherwise send back an error
+  app.post("/api/signup", (req, res) => {
+    console.log(req.user);
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then(() => {
+        res.redirect(307, "/api/login");
+      })
+      .catch((err) => {
+        res.status(401).json(err);
+      });
+  });
+
   app.get("/api/:team", async (req, res) => {
     const deck = await db.req.params.team.findAll({});
     console.log(deck);

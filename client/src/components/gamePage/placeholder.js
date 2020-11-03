4cd24829d036c5669fdb134405e269116489578b
Mohammed <mohammedyasirsuara@cmail.carleton.ca>
2020-11-03 12:23:20 -0500

    Conditionally rendering play cards

--- client/src/components/gamePage/placeholder.js
+++ client/src/components/gamePage/placeholder.js
@@ -1,16 +1,44 @@
 /* eslint-disable */
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
+import heat from '../../images/Heat.png'
+import lakers from '../../images/Lakers.png'
+import rockets from '../../images/Rockets.png'
+import raptors from '../../images/Raptors.png'
 
-class CardHolder extends Component {
-  handleClick() {
-    this.props.changePlayer(this.props.player);
-  }
+function CardHolder (props) {
+    // console.log(props.player)
+    const [cardCol, setCardCol]=useState('')
+    const [cardImg, setImg]=useState('')
 
-  render() {
+    const teams ={
+      lakers: {
+        color:'linear-gradient(gold, purple)',
+        image: lakers
+    },
+      raptors: {
+        color:'linear-gradient(#CE1141,#000000,#A1A1A4)',
+        image: raptors
+    },
+      heat: {
+        color:'linear-gradient(#98002E, #F9A01B, #000000)',
+        image: heat
+      },
+      rockets: {
+        color:'linear-gradient(#C4CED4, #CE1141, #000000)',
+        image: rockets
+      },
+    }
+    useEffect(()=>{
+      setCardCol(teams[props.player].color)
+      setImg(teams[props.player].image)
+    },[])
+
+    function handleClick(){
+      console.log(props)
+    }
     return (
-      <div className={this.props.className} onClick={this.handleClick} />
+      <div className={props.className} style={{backgroundImage:`url(${cardImg})`}}/>
     );
-  }
 }
 
 export default CardHolder;
--- client/src/pages/Play.js
+++ client/src/pages/Play.js
@@ -2,6 +2,8 @@
 
 import React, { useEffect, useState } from "react";
 import API from "../utils/API";
+import Placeholder from "../components/gamePage/placeholder";
+import PlayerCard from "../components/gamePage/playerCard"
 
 function Play(props) {
   const [game, setGame] = useState({
@@ -20,12 +22,29 @@ function Play(props) {
 
   useEffect(() => {
     console.log("loaded")
+    console.log(game.player1.team, game.player2.team)
   },[game.player1, game.player2])
+
+  function clickHandler(){
+
+  }
 
   return (
   <>
   <h1>Game Page</h1>
   <h2>{props.location.state.player1} vs {props.location.state.player2}</h2>
+  <div>
+  <Placeholder
+          player={game.player1.team}
+          className="playCard1"
+          // handleClick={this.clickHandler}
+  />
+  <Placeholder
+          player={game.player2.team}
+          className="playCard2"
+          // handleClick={this.clickHandler}
+  />
+  </div>
     </>
   );
 }

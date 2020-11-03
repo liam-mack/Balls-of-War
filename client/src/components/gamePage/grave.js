e8c5e0a65e389a8f514c36ef0a48a5af5d9bdff3
Mohammed <mohammedyasirsuara@cmail.carleton.ca>
2020-11-03 17:32:57 -0500

    Added graveyard component

---
+++ client/src/components/gamePage/grave.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React, { Component, useEffect, useState } from "react";
+import './graveyard.css';
+
+function Graveyard (props) {
+
+    return (
+      <div className={`${props.className}Grave`}/>
+    );
+}
+
+export default Graveyard;

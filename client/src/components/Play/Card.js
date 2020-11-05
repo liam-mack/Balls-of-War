315fd6cc91673d67a733feff23f376b7e3d1ee80
shamik05 <64784024+shamik05@users.noreply.github.com>
2020-11-05 02:30:28 -0500

    added logic on stat comparison

--- client/src/components/Play/Card.js
+++ client/src/components/Play/Card.js
@@ -1,31 +1,28 @@
 /* eslint-disable */
-import React, { Component } from "react";
+import React from "react";
 import "./card.scss";
 // import avatar from "../../images/Card/";
 
 // function Card ({ name, position, jersey, height, weight, points, fieldgoal, rebounds, assists, personalfouls, turnovers, onClick }) {
-function Card ({ name, position, jersey, height, weight, points, fieldgoal, rebounds, assists, personalfouls, turnovers, onClick }) {
-  // console.log(props);
+function Card ({ name, position, jersey, height, weight, points, fieldgoal, rebounds, assists, personalfouls, turnovers, onClick, team, player }) {
 
     return (
       <div className="playerCard1">
-
-        <h4 onClick= { (event) => onClick(event.target) }>{ name }</h4>
-        <h4 onClick= { (event) => onClick(event.target) }>{ position }</h4>
-        <h4 onClick= { (event) => onClick(event.target) }>{ jersey }</h4>
+        <h4 onClick= { (event) => onClick(event.target) }>{ name } </h4>
+        <h4 onClick= { (event) => onClick(event.target) }>{ position } {player}</h4>
+        <h4 onClick= { (event) => onClick(event.target) }>{ jersey } {team}</h4>
 
         {/* < PlayerStats /> */}
-        <h4 className="playerCard1__height stat" onClick= { (event) => onClick(event.target) }> { height} </h4>
-        <h4 className="playerCard1__weight stat" onClick= { (event) => onClick(event.target) }> { weight} </h4>
-        <h4 className="playerCard1__points stat" onClick= { (event) => onClick(event.target) }> { points} </h4>
-        <h4 className="playerCard1__fieldgoal stat" onClick= { (event) => onClick(event.target) }> { fieldgoal} </h4>
-        <h4 className="playerCard1__rebounds stat" onClick= { (event) => onClick(event.target) }> { rebounds} </h4>
-        <h4 className="playerCard1__assists stat" onClick= { (event) => onClick(event.target) }> { assists} </h4>
-        <h4 className="playerCard1__personalfouls stat" onClick= { (event) => onClick(event.target) }> { personalfouls } </h4>
-        <h4 className="playerCard1__turnovers stat" onClick = { (event) => onClick(event.target) }> { turnovers}</h4>
+        <h4 id="height" className="playerCard1__height stat" onClick= { (event) => onClick(event.target) }> height { height} </h4>
+        <h4 id="weight" className="playerCard1__weight stat" onClick= { (event) => onClick(event.target) }> weight { weight} </h4>
+        <h4 id="points" className="playerCard1__points stat" onClick= { (event) => onClick(event.target) }> points { points} </h4>
+        <h4 id="fieldgoal" className="playerCard1__fieldgoal stat" onClick= { (event) => onClick(event.target) }> fieldgoal { fieldgoal} </h4>
+        <h4 id="rebounds" className="playerCard1__rebounds stat" onClick= { (event) => onClick(event.target) }> rebounds { rebounds} </h4>
+        <h4 id="assists" className="playerCard1__assists stat" onClick= { (event) => onClick(event.target) }> assists { assists} </h4>
+        <h4 id="personalfouls" className="playerCard1__personalfouls stat" onClick= { (event) => onClick(event.target) }> personalfouls {personalfouls} </h4>
+        <h4 id="turnovers" className="playerCard1__turnovers stat" onClick = { (event) => onClick(event.target) }> turnovers {turnovers}</h4>
       </div>
     )
-
 }
 
 export default Card;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,28 +1,22 @@
-/* eslint-disable */
 import axios from "axios";
 
 export default {
-  // Get Players
-  async getPlayers(team) {
-    return await axios.get(`/api/${team}`);
-  },
-  async postPlayers() {
-    return axios.post("/api/lakers");
-  },
   async signUp(data) {
-    console.log(data)
-    return await axios.post("/api/signup", data);
+    console.log(data);
+    return axios.post("/api/signup", data);
   },
+  // Create game
   async createGame(players) {
     return axios.post("/api/game", players);
   },
+  // Get game id
   async getGame(id) {
     console.log(id);
-    return await axios.get(`/api/game/${id.session}`);
+    return axios.get(`/api/game/${id.session}`);
   },
-  async playGame(id,method,stat) {
+  // Play game with an action
+  async playGame(id, method, stat) {
     console.log(method, id, stat);
-    return await axios.put(`/api/game/${id}/${method}`, {stat} )
-  }
-   
+    return axios.put(`/api/game/${id}/${method}`, { stat });
+  },
 };

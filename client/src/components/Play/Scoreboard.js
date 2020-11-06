cda07aecdde8354d0e6e85cac2d95fd9be504176
liam-mack <liammma9@gmail.com>
2020-11-06 15:47:22 -0500

    Scoreboard

--- client/src/components/Play/Scoreboard.js
+++ client/src/components/Play/Scoreboard.js
@@ -1,10 +1,45 @@
+/* eslint-disable */
 import React from "react";
+import Lakers from "../../images/Lakers.png";
+import Heat from "../../images/Heat.png";
+import Rockets from "../../images/Rockets.png";
+import Raptors from "../../images/Raptors.png";
+import "./scoreboard.scss"
 
 export default function Scoreboard({ gameState }) {
-//   console.log(gameState.player1);
+  let homeLogo;
+  let awayLogo;
+switch (gameState.player1.team) {
+  case "Lakers":
+    homeLogo = Lakers;
+    break;
+  case "Heat":
+    homeLogo = Heat;
+    break;
+  case "Raptors":
+    homeLogo = Raptors;
+    break;
+  case "Rockets":
+    homeLogo = Rockets;
+    break;
+}
+switch (gameState.player2.team) {
+  case "Lakers":
+    awayLogo = Lakers;
+    break;
+  case "Heat":
+    awayLogo = Heat;
+    break;
+  case "Raptors":
+    awayLogo = Raptors;
+    break;
+  case "Rockets":
+    awayLogo = Rockets;
+    break;
+}
   return (
     <div className="scoreboard">
-      Generic Scoreboard between {gameState.player1.team} and {gameState.player2.team};
+      Generic Scoreboard {gameState.player1.team} <img alt="homeLogo" src={homeLogo} className="scoreboard__Logo"/>  {gameState.player2.team} <img alt="awayLogo" src={awayLogo} className="scoreboard__Logo"/>
     </div>
   );
 }

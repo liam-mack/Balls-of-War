1b95bdd1dc66f0931c157397b989d95384837022
liam-mack <liammma9@gmail.com>
2020-11-06 04:28:54 -0500

    Card Render and Scoreboard Component

--- client/src/pages/Opponent.js
+++ client/src/pages/Opponent.js
@@ -1,26 +1,27 @@
+/* eslint-disable */
 import React from "react";
+import Card from "../components/Play/Card"
+import images from "../images/Card/Images";
 
 function Opponent({
   name, position, jersey, height, weight, points, fieldgoal, rebounds, assists,
   personalfouls, turnovers, team, player,
 }) {
   return (
-    <div>
-      <h4>{ name } </h4>
-      <h4>{ position } {player}</h4>
-      <h4>{ jersey } {team}</h4>
-
-      {/* < PlayerStats /> */}
-      <h4 id="height"> height { height} </h4>
-      <h4 id="weight"> weight { weight} </h4>
-      <h4 id="points"> points { points} </h4>
-      <h4 id="fieldgoal"> fieldgoal { fieldgoal} </h4>
-      <h4 id="rebounds"> rebounds { rebounds} </h4>
-      <h4 id="assists"> assists { assists} </h4>
-      <h4 id="personalfouls"> personalfouls {personalfouls} </h4>
-      <h4 id="turnovers"> turnovers {turnovers}</h4>
-    </div>
-  );
+    <Card name = {name}
+    position = {position}
+    jersey = {jersey}
+    height = {height}
+    weight = {weight} 
+    points = {points}
+    fieldgoal = {fieldgoal}
+    rebounds = {rebounds}
+    assists = {assists}
+    personalfouls = {personalfouls}
+    turnovers = {turnovers}
+    team = {team}
+    player = {player}/>
+  )
 }
 
 export default Opponent;
